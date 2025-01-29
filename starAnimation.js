255d1f5d55674fec43d47ad2399cd91943bfd565
jakubekgranie <kawkagaming@interia.pl>
2025-01-29 21:30:04 +0100

    ✧ Update 29/01/25
    - Removed unused assets;
    - Optimized image assets;
    - Continued development.

--- starAnimation.js
+++ starAnimation.js
@@ -1,32 +1,22 @@
 const styles = [
-                ["#ffdbc4", "#cfecb0", "#a0f5c7"]
+                ["#ffdbc4", "#a0f5c7", "#7ff7b7"]
             ];
 let listeners = [],
     lock = false;
-function animate(styleID){
+function animate(){
     document.getElementById("star").classList.add("star-color0-support");
     for(let i = 0; i < 53; i++)
         for(let j = 0; j < 100; j++){
             const element = document.getElementById(`unit-${i}-${j}`);
             if(typeof element !== "undefined" && element != null){
                 element.style.animationDelay = `${0.005 * j + 0.005 * i}s`;
+                setTimeout(() => element.style.color = styles[0][2], 5 * j + 5 * i);
                 element.classList.add("star-color0");
             }
         }
-    document.getElementById("star").style.filter = `drop-shadow(0px 0px 0.25rem ${styles[0][0]})`;
-    document.getElementById("star").style.textShadow = `0 0 0.75rem ${styles[0][2]}`;
-    /*document.getElementById("star").style.filter = `drop-shadow(0px 0px 0.25rem ${styles[0][0]})`;
-    document.getElementById("star").style.textShadow = `0 0 0.75rem ${styles[0][2]}`;
-    for(let i = 0; i < 53; i++)
-        for(let j = 0; j < 100; j++){
-            const element = document.getElementById(`unit-${i}-${j}`);
-            if(typeof element !== "undefined" && element != null)
-                for(let k = 0; k < 3; k++){
-                    const timeout = 2 * i + 2 * j + 25 * (k + 1 * k + 1);
-                    setTimeout(() => {element.style.color = styles[styleID][k];}, timeout);
-                }
-        }*/
-        
+    const star = document.getElementById("star");
+    star.style.filter = `drop-shadow(0px 0px 0.25rem ${styles[0][0]})`;
+    star.style.textShadow = `0 0 0.75rem ${styles[0][1]}`;
 }
 function initialize(){
     for(let i = 0; i < 53; i++){
