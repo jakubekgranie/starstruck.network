2c8877ef9567ce7c13c83152b6617ea3bedd6f90
jakubekgranie <kawkagaming@interia.pl>
2025-02-03 19:57:27 +0100

    ✧ Update 03/02/25
    - Beautified code;
    - Improved the aesthetics of the Spotify tab;
    - Added new activity icons.

--- lanyard.js
+++ lanyard.js
@@ -1,9 +1,13 @@
+// https://www.npmjs.com/package/color.js
+var ColorJS = function (r) { "use strict"; var a = (r, a) => { var t = r.map((r => { var t = Array.isArray(r) ? r : r.split(",").map(Number); return "hex" === a.format ? e(t) : t })); return 1 === a.amount || 1 === t.length ? t[0] : t }, t = (r, a) => { var t = Math.round(r / a) * a; return Math.min(t, 255) }, e = r => "#" + r.map((r => { var a = r.toString(16); return 1 === a.length ? "0" + a : a })).join(""), n = (r, t) => { for (var e = 4 * t.sample, n = r.length / e, o = { r: 0, g: 0, b: 0 }, g = 0; g < r.length; g += e)o.r += r[g], o.g += r[g + 1], o.b += r[g + 2]; return a([[Math.round(o.r / n), Math.round(o.g / n), Math.round(o.b / n)]], t) }, o = (r, e) => { for (var n = 4 * e.sample, o = {}, g = 0; g < r.length; g += n) { var m = [t(r[g], e.group), t(r[g + 1], e.group), t(r[g + 2], e.group)].join(); o[m] = o[m] ? o[m] + 1 : 1 } return a(Object.entries(o).sort((([r, a], [t, e]) => a > e ? -1 : 1)).slice(0, e.amount).map((([r]) => r)), e) }, g = (r, a, t) => new Promise(((e, n) => { return (o = (r => "string" == typeof r ? r : r.src)(a), new Promise(((r, a) => { var t = document.createElement("canvas"), e = t.getContext("2d"), n = new Image; n.onload = () => { t.height = n.height, t.width = n.width, e.drawImage(n, 0, 0); var a = e.getImageData(0, 0, n.width, n.height).data; r(a) }, n.onerror = () => a(Error("Image loading failed.")), n.crossOrigin = "", n.src = o }))).then((a => e(r(a, (({ amount: r = 3, format: a = "array", group: t = 20, sample: e = 10 } = {}) => ({ amount: r, format: a, group: t, sample: e }))(t))))).catch((r => n(r))); var o })); return r.average = (r, a) => g(n, r, a), r.prominent = (r, a) => g(o, r, a), r }({});
+
 const CONFIG = {
     bannedActivities: ["Custom Status", "Spotify"],
     resources_location: "\\Visual assets\\",
     imagery: {
         "Wuthering Waves": "wuwa.png",
-        "Microsoft Visual Studio": "vs2022.png"
+        "Microsoft Visual Studio": "vs2022.png",
+        "VALORANT": "valorant.png"
     },
     max_length: 41,
     wrapper_length: 17,
@@ -12,73 +16,95 @@ const CONFIG = {
 let currentActivities = [],
     debounceReference = null,
     firstUpdate = true;
-function parseLanyard2(payload){
+
+function parseLanyard2(payload) { // the main websocket operator
     console.log("[Lanyard2]\n", payload);
-    if(payload.listening_to_spotify){ // spotify tab section
+    if (payload.listening_to_spotify) { // spotify tab section
         const spotify = payload.spotify, // prevent songs from mixing
             classHeader = "spotify-tab-",
             elementData = [ // spotify tab element data, 0 => ids, 1 => content
-                                ["album-art", "author", "hyperlink", "header", "title", "album"], 
-                                [spotify.album_art_url, payload.spotify.artist, "https://open.spotify.com/track/", "Listening to Spotify", spotify.song, `🖸 ${spotify.album}`]
-                            ];
+                ["album-art", "author", "hyperlink", "header", "title", "album"],
+                [spotify.album_art_url, payload.spotify.artist, "https://open.spotify.com/track/", "Listening to Spotify", spotify.song, `🖸 ${spotify.album}`]
+            ];
+        function setGradient(groups = 100) { // sets the gradient
+            ColorJS.prominent(elementData[1][0], { amount: 3, group: groups }).then(color => {
+                if (groups == 0 && color.length != 3)
+                    return;
+                if (color.length != 3) {
+                    setGradient(groups - 20);
+                    return;
+                }
+                color.sort();
+                let gradient = `linear-gradient(to right bottom, `,
+                    gradientIterations = 4;
+                for (let i = 0; i < gradientIterations; i++) {
+                    gradient += "rgb(";
+                    for (let j = 0; j < 3; j++)
+                        gradient += `${Math.floor((color[0][j] * (1 - i * 0.25) + color[1][j] * i * 0.25))}, `;
+                    gradient += "0.2), ";
+                }
+                for (let i = gradientIterations; i > - 1; i--) {
+                    gradient += "rgba(";
+                    for (let j = 0; j < 3; j++)
+                        gradient += `${Math.floor((color[2][j] * (1 - i * 0.25) + color[1][j] * i * 0.25))}, `;
+                    gradient += "0.2), ";
+                }
+                gradient = gradient.substring(0, gradient.length - 2) + ")";
+                gradientBlock.style.backgroundImage = gradient;
+            });
+        }
+        const gradientBlock = document.getElementById("spotify-tab-gradient-block");
+        ColorJS.average(elementData[1][0]).then(color =>
+            gradientBlock.style.borderColor = `rgb(${color[0]}, ${color[1]}, ${color[2]}`
+        );
+        setGradient();
         document.getElementById(`${classHeader}${elementData[0][0]}`).src = elementData[1][0];
-        let authors = elementData[1][1].split("; "), 
+        let authors = elementData[1][1].split("; "),
             author = `by <strong>${authors[0]}</strong>`;
-        for(let i = 1; i < authors.length; i++){ // format authors
-            if(`${author}, <strong>${authors[i]}</strong>`.length > CONFIG.max_length + i * CONFIG.wrapper_length) // 17 equals tag length, 24 + 17 = 41
+        for (let i = 1; i < authors.length; i++) { // format authors
+            if (`${author}, <strong>${authors[i]}</strong>`.length > CONFIG.max_length + i * CONFIG.wrapper_length) // 17 equals tag length, 24 + 17 = 41
                 break;
             author += `, <strong>${authors[i]}</strong>`;
         }
         document.getElementById(`${classHeader}${elementData[0][1]}`).innerHTML = author;
         document.getElementById(`${classHeader}${elementData[0][2]}`).href = `${elementData[1][2]}${spotify.track_id}`; // create the spotify album link
-        for(let i = 3; i < elementData[0].length; i++)
+        for (let i = 3; i < elementData[0].length; i++)
             document.getElementById(`${classHeader}${elementData[0][i]}`).innerHTML = elementData[1][i]; // standard content injection
-            // [ADD] a timeout changing the tab to blank after a select period with no updates
+        // [ADD] a timeout changing the tab to blank after a select period with no updates
     }
-    const activities = payload.activities; // shorthand, prevent arbitrary changes
-    if(activities.length > 0){
-        for(let i = 0; i < activities.length; i++)
-            for(let j = 0; j < CONFIG.bannedActivities.length; j++)
-                if(activities[i].name == CONFIG.bannedActivities[j]){
-                    activities.splice(i, 1); // remove banned activities
-                    i--;
-                    break;
-                }
-        for(let i = currentActivities.length - 1; i > -1; i--){ // remove absent activities
+    let activities = payload.activities; // shorthand, prevent arbitrary changes
+    if (activities.length > 0) {
+        activities = activities.filter(activity => !CONFIG.bannedActivities.includes(activity.name));
+        for (let i = currentActivities.length - 1; i > -1; i--) { // remove absent activities
             let includes = false;
-            for(let j = 0; j < activities.length; j++){
-                if(currentActivities[i] == activities[j].name + (activities[j].details ? ` - ${activities[j].details}` : ``))
+            for (let j = 0; j < activities.length; j++) {
+                if (currentActivities[i] == activities[j].name + (activities[j].details ? ` - ${activities[j].details}` : ``))
                     includes = true;
             }
-            if(!includes){
+            if (!includes) {
                 const element = document.getElementById(currentActivities[i]);
                 element.classList.add("hidden-activity-tab-icon", "transparent");
-                setTimeout(() => {element.remove();}, 350); // lesson learned
+                setTimeout(() => { element.remove(); }, 350); // lesson learned
                 currentActivities.splice(i, 1);
             }
         }
         const trueActivityTab = document.getElementById("activity-tab"),
-                activityTitle = document.getElementById("activity-tab-title");
-        if(currentActivities.length == 0)
+            activityTitle = document.getElementById("activity-tab-title");
+        if (currentActivities.length == 0)
             trueActivityTab.classList.remove("show-activity-tab");
-        let activitiesAvailable = (currentActivities.length != 0) ? true : false; // considers continuing activities
+        let activitiesAvailable = currentActivities.length != 0; // considers continuing activities
         const fragment = document.createDocumentFragment(), // limit reflows
-                maxAmount = 6 - currentActivities.length; // 6 being the maximum amount of icons onscreen - the ones currently on display
-        for(let i = 0; i < activities.length && i < maxAmount; i++){
+            maxAmount = 6 - currentActivities.length; // 6 being the maximum amount of icons onscreen - the ones currently on display
+        for (let i = 0; i < activities.length && i < maxAmount; i++) {
             const activity = activities[i], // shorthand
-            identifier = activity.name + (activity.details ? ` - ${activity.details}` : ``); // element's id
-            /*
-                Check if the activity:
-                    1 => features the required data (large_image from assets);
-                    2 => isn't featured already.
-            */
-            if(!currentActivities.includes(identifier)){
+                identifier = activity.name + (activity.details ? ` - ${activity.details}` : ``); // element's id
+            if (!currentActivities.includes(identifier)) {
                 let source = `${CONFIG.resources_location}missing-file.png`;
-                if(typeof activity.assets !== "undefined" && typeof activity.assets.large_image !== "undefined")
+                if (typeof activity.assets !== "undefined")
                     source = `https://cdn.discordapp.com/app-assets/${encodeURIComponent(activity.application_id)}/${encodeURIComponent(activity.assets.large_image)}.png`;
-                if(activity.name == "Visual Studio Code" && (activity.assets.large_text == "Idling")) // special case
+                if (activity.name == "Visual Studio Code" && typeof activity.assets.large_text !== "undefined" && activity.assets.large_text == "Idling") // special case
                     source = `${CONFIG.resources_location}vsc.png`;
-                else if(CONFIG.imagery[activity.name]) // replace with a custom icon for cosmetic or replacement purposes
+                else if (CONFIG.imagery[activity.name]) // replace with a custom icon for cosmetic or replacement purposes
                     source = `${CONFIG.resources_location}${CONFIG.imagery[activity.name]}`;
                 currentActivities.push(identifier);
                 activitiesAvailable = true;
@@ -87,31 +113,27 @@ function parseLanyard2(payload){
                 activityIcon.classList.add("activity-tab-icon", "sidebar-image-decor");
                 activityIcon.alt = activity.name;
                 activityIcon.title = activityIcon.id = identifier;
-                if(source == `${CONFIG.resources_location}missing-file.png`)
+                if (source === `${CONFIG.resources_location}missing-file.png`)
                     activityIcon.style.imageRendering = "pixelated";
                 fragment.appendChild(activityIcon);
             }
         }
         document.getElementById("activity-tab-feed").appendChild(fragment);
-        if(activitiesAvailable){
+        if (activitiesAvailable) {
             trueActivityTab.style.display = "flex";
             trueActivityTab.classList.add("show-activity-tab");
             activityTitle.style.marginBottom = "0.6rem";
         }
-        else{
+        else {
             trueActivityTab.style.display = null;
             trueActivityTab.classList.remove("show-activity-tab");
             activityTitle.style.marginBottom = null;
         }
     }
 }
-async function lanyard2(){
+async function lanyard2() {
     const socket = new WebSocket("wss://api.lanyard.rest/socket");
     let heartbeat;
-    if (socket && socket.readyState === WebSocket.OPEN) {
-        console.warn("[Lanyard2] WebSocket is already open.");
-        return;
-    }
     socket.addEventListener("open", () => {
         socket.send(
             JSON.stringify({
@@ -132,15 +154,15 @@ async function lanyard2(){
     });
 
     socket.addEventListener("message", ({ data }) => {
-        const { t : state, d : payload } = JSON.parse(data);
-        if (state === "INIT_STATE" || state === "PRESENCE_UPDATE"){
+        const { t: state, d: payload } = JSON.parse(data);
+        if (state === "INIT_STATE" || state === "PRESENCE_UPDATE") {
             clearTimeout(debounceReference);
-            if(firstUpdate){
+            if (firstUpdate) {
                 parseLanyard2(payload);
                 firstUpdate = false;
             }
             else
-                debounceReference = setTimeout(() => {parseLanyard2(payload)}, CONFIG.debounceTimeout);
+                debounceReference = setTimeout(() => { parseLanyard2(payload) }, CONFIG.debounceTimeout);
         }
     });
 
@@ -152,10 +174,10 @@ async function lanyard2(){
                 console.log("[Lanyard2] Trying to reconnect...");
                 lanyard2();
             }, 1000);
-        } catch(err) {
+        } catch (err) {
             console.error("[Lanyard2] An unexpected error occured near socket closure.\n\n" + err);
         }
         console.warn("[Lanyard2]\n", event);
-      };
+    };
 }
 document.addEventListener("DOMContentLoaded", lanyard2);
--- starAnimation.js
+++ starAnimation.js
@@ -1,14 +1,13 @@
 const styles = [
-                ["#ffdbc4", "#a0f5c7", "#7ff7b7"]
-            ];
-let lock = false;
+    ["#ffdbc4", "#a0f5c7", "#7ff7b7"]
+];
 
-function animate(){
+function animate() {
     document.getElementById("star").classList.add("star-color0-support");
-    for(let i = 0; i < 53; i++)
-        for(let j = 0; j < 100; j++){
+    for (let i = 0; i < 53; i++)
+        for (let j = 0; j < 100; j++) {
             const element = document.getElementById(`unit-${i}-${j}`);
-            if(typeof element !== "undefined" && element != null){
+            if (typeof element !== "undefined" && element != null) {
                 element.style.animationDelay = `${0.005 * j + 0.005 * i}s`;
                 setTimeout(() => element.style.color = styles[0][2], 5 * j + 5 * i);
                 element.classList.add("star-color0");
@@ -19,26 +18,36 @@ function animate(){
     star.style.textShadow = `0 0 0.75rem ${styles[0][1]}`;
 }
 
-function animateName(){
+function animateName() {
     let name = "starstruck",
-        limit = 0,
-        randomOutput;
+        letterIndex = -1;
+    const feed = document.getElementById("name-header-feed"),
+        fragment = document.createDocumentFragment();
+    for (let i = 0; i < name.length; i++) {
+        const element = document.createElement("span");
+        element.innerHTML = String.fromCharCode(Math.floor(Math.random() * 62) + 65);
+        element.classList.add("name-header-random");
+        fragment.appendChild(element);
+    }
+    feed.appendChild(fragment);
     const interval = setInterval(() => {
-        if(++limit % 2 == 1){
-            randomOutput = "";
-            for(let i = 0; i < name.length; i++)
-                randomOutput += String.fromCharCode(Math.floor(Math.random() * 62) + 65);
+        const letter = document.querySelector(".name-header-random");
+        letter.innerHTML = name[++letterIndex];
+        letter.classList.remove("name-header-random");
+        if (letterIndex % 2 == 1) {
+            const lettersToRandomize = document.querySelectorAll(".name-header-random");
+            for (let i = 0; i < lettersToRandomize.length; i++)
+                lettersToRandomize[i].innerHTML = String.fromCharCode(Math.floor(Math.random() * 62) + 65);
         }
-        document.getElementById("name-header-feed").innerHTML = name.substring(0, limit) + randomOutput.substring(limit, randomOutput.length);
-        if(limit == name.length) {
+        if (letterIndex == name.length - 1) {
             clearInterval(interval);
             document.getElementById("name-header-underscore").classList.add("blink-forever");
         }
     }, 40);
 }
 
-function initialize(){
-    document.getElementById("spotify-tab-hyperlink").addEventListener("mouseover", () => {animate()});
+function initialize() {
+    animate();
     animateName();
 }
 document.addEventListener("DOMContentLoaded", initialize);
