a32f82bf05353a294b7178eee86344a7a6211d4f
jakubekgranie <kawkagaming@interia.pl>
2025-02-05 12:20:58 +0100

    ✧ Update 05/02/25
    - Added a Spotify tab song counter;
    - Added JavaScript commentary;
    - Added an activity update system;
    - Added a new activity icon (CurseForge).

--- lanyard.js
+++ lanyard.js
@@ -4,30 +4,43 @@ var ColorJS = function (r) { "use strict"; var a = (r, a) => { var t = r.map((r 
 const CONFIG = {
     bannedActivities: ["Custom Status", "Spotify"],
     resources_location: "\\Visual assets\\",
-    imagery: {
+    imagery: { // custom activity icons
         "Wuthering Waves": "wuwa.png",
         "Microsoft Visual Studio": "vs2022.png",
-        "VALORANT": "valorant.png"
+        "VALORANT": "valorant.png",
+        "CurseForge": "curseforge.png",
     },
-    max_length: 41,
-    wrapper_length: 17,
+    max_length: 41, // max title length
+    wrapper_length: 17, // title wrapper length to be substracted from the total
     debounceTimeout: 250
 };
+const elementSuffixes = ["album-art", "author", "hyperlink", "header", "title", "album"],
+    classHeader = "spotify-tab-";
 let currentActivities = [],
     debounceReference = null,
-    firstUpdate = true;
+    firstUpdate = true,
+    countData = ["", 0, 1];
 
 function parseLanyard2(payload) { // the main websocket operator
     console.log("[Lanyard2]\n", payload);
     if (payload.listening_to_spotify) { // spotify tab section
         const spotify = payload.spotify, // prevent songs from mixing
-            classHeader = "spotify-tab-",
-            elementData = [ // spotify tab element data, 0 => ids, 1 => content
-                ["album-art", "author", "hyperlink", "header", "title", "album"],
-                [spotify.album_art_url, payload.spotify.artist, "https://open.spotify.com/track/", "Listening to Spotify", spotify.song, `🖸 ${spotify.album}`]
-            ];
+            elementData = [spotify.album_art_url, payload.spotify.artist, "https://open.spotify.com/track/", "Listening to Spotify", spotify.song, `🖸 ${spotify.album}`];
+        const counter = document.getElementById("spotify-tab-count");
+        if (countData[0] == elementData[4] && countData[1] != spotify.timestamps.start) { // counter
+            countData[2]++;
+            countData[1] = spotify.timestamps.start;
+            if (countData[2] > 2) {
+                counter.innerHTML = `x${countData[2]}`;
+                counter.style.opacity = "1";
+            }
+        }
+        else {
+            countData = [elementData[4], 0, 1];
+            counter.style = null;
+        }
         function setGradient(groups = 100) { // sets the gradient
-            ColorJS.prominent(elementData[1][0], { amount: 3, group: groups }).then(color => {
+            ColorJS.prominent(elementData[0], { amount: 3, group: groups }).then(color => {
                 if (groups == 0 && color.length != 3)
                     return;
                 if (color.length != 3) {
@@ -54,28 +67,36 @@ function parseLanyard2(payload) { // the main websocket operator
             });
         }
         const gradientBlock = document.getElementById("spotify-tab-gradient-block");
-        ColorJS.average(elementData[1][0]).then(color =>
+        ColorJS.average(elementData[0]).then(color =>
             gradientBlock.style.borderColor = `rgb(${color[0]}, ${color[1]}, ${color[2]}`
         );
         setGradient();
-        document.getElementById(`${classHeader}${elementData[0][0]}`).src = elementData[1][0];
-        let authors = elementData[1][1].split("; "),
+        document.getElementById(`${classHeader}${elementSuffixes[0]}`).src = elementData[0];
+        let authors = elementData[1].split("; "),
             author = `by <strong>${authors[0]}</strong>`;
         for (let i = 1; i < authors.length; i++) { // format authors
             if (`${author}, <strong>${authors[i]}</strong>`.length > CONFIG.max_length + i * CONFIG.wrapper_length) // 17 equals tag length, 24 + 17 = 41
                 break;
             author += `, <strong>${authors[i]}</strong>`;
         }
-        document.getElementById(`${classHeader}${elementData[0][1]}`).innerHTML = author;
-        document.getElementById(`${classHeader}${elementData[0][2]}`).href = `${elementData[1][2]}${spotify.track_id}`; // create the spotify album link
-        for (let i = 3; i < elementData[0].length; i++)
-            document.getElementById(`${classHeader}${elementData[0][i]}`).innerHTML = elementData[1][i]; // standard content injection
-        // [ADD] a timeout changing the tab to blank after a select period with no updates
+        document.getElementById(`${classHeader}${elementSuffixes[1]}`).innerHTML = author;
+        document.getElementById(`${classHeader}${elementSuffixes[2]}`).href = `${elementData[2]}${spotify.track_id}`; // create the spotify album link
+        for (let i = 3; i < elementSuffixes.length; i++)
+            document.getElementById(`${classHeader}${elementSuffixes[i]}`).innerHTML = elementData[i]; // standard content injection
+
+    }
+    else {
+        const defaultElementContent = ["spotify.png", "N/A", null, "Listening to Spotify", "Player stopped", "🖸 N/A"];
+        document.getElementById(`${classHeader}${elementSuffixes[0]}`).src = `${CONFIG.resources_location}${defaultElementContent[0]}`;
+        document.getElementById(`${classHeader}${elementSuffixes[1]}`).innerHTML = defaultElementContent[1];
+        document.getElementById(`${classHeader}${elementSuffixes[2]}`).href = defaultElementContent[2];
+        for (let i = 3; i < elementSuffixes.length; i++)
+            document.getElementById(`${classHeader}${elementSuffixes[i]}`).innerHTML = defaultElementContent[i];
     }
     let activities = payload.activities; // shorthand, prevent arbitrary changes
     if (activities.length > 0) {
         activities = activities.filter(activity => !CONFIG.bannedActivities.includes(activity.name));
-        for (let i = currentActivities.length - 1; i > -1; i--) { // remove absent activities
+        for (let i = currentActivities.length - 1; i > -1; i--) { // remove absent activities activities don't update   
             let includes = false;
             for (let j = 0; j < activities.length; j++) {
                 if (currentActivities[i] == activities[j].name + (activities[j].details ? ` - ${activities[j].details}` : ``))
@@ -98,28 +119,38 @@ function parseLanyard2(payload) { // the main websocket operator
         for (let i = 0; i < activities.length && i < maxAmount; i++) {
             const activity = activities[i], // shorthand
                 identifier = activity.name + (activity.details ? ` - ${activity.details}` : ``); // element's id
-            if (!currentActivities.includes(identifier)) {
+            if (typeof activity.assets !== "undefined") {
                 let source = `${CONFIG.resources_location}missing-file.png`;
-                if (typeof activity.assets !== "undefined")
+                if (typeof activity.assets.large_image !== "undefined" && activity.assets.large_image.match(/^\d+$/))
                     source = `https://cdn.discordapp.com/app-assets/${encodeURIComponent(activity.application_id)}/${encodeURIComponent(activity.assets.large_image)}.png`;
-                if (activity.name == "Visual Studio Code" && typeof activity.assets.large_text !== "undefined" && activity.assets.large_text == "Idling") // special case
+                if (activity.name == "Visual Studio Code" && typeof activity.assets.large_text !== "undefined" && activity.assets.large_text === "Idling") // special case
                     source = `${CONFIG.resources_location}vsc.png`;
                 else if (CONFIG.imagery[activity.name]) // replace with a custom icon for cosmetic or replacement purposes
                     source = `${CONFIG.resources_location}${CONFIG.imagery[activity.name]}`;
-                currentActivities.push(identifier);
-                activitiesAvailable = true;
-                const activityIcon = document.createElement("img");
-                activityIcon.src = source;
-                activityIcon.classList.add("activity-tab-icon", "sidebar-image-decor");
-                activityIcon.alt = activity.name;
-                activityIcon.title = activityIcon.id = identifier;
-                if (source === `${CONFIG.resources_location}missing-file.png`)
-                    activityIcon.style.imageRendering = "pixelated";
-                fragment.appendChild(activityIcon);
+
+                let activityIcon;
+                if (!currentActivities.includes(identifier)) {
+                    const toIterate = [["src", "alt", "title", "id", "class", "style"], [source, activity.name, identifier, identifier, "activity-tab-icon sidebar-image-decor"]]
+                    activityIcon = document.createElement("img");
+                    if (source === `${CONFIG.resources_location}missing-file.png`)
+                        values.push("image-rendering: pixelated;");
+                    activitiesAvailable = true;
+                    for(let j = 0; j < toIterate[1].length; j++)
+                        activityIcon.setAttribute(toIterate[0][j], toIterate[1][j]);
+                    currentActivities.push(identifier);
+                    fragment.appendChild(activityIcon);
+                }
+                else { // update present elements' images
+                    activityIcon = document.getElementById(currentActivities[i]);
+                    if (activityIcon.src != source && !CONFIG.imagery[activityIcon.alt]) // ensure the activity does not have a custom profile
+                        activityIcon.src = source;
+                    activityIcon.alt = activity.name;
+                    activityIcon.title = activity.name + (activity.details ? ` - ${activity.details}` : ``);
+                }
             }
         }
-        document.getElementById("activity-tab-feed").appendChild(fragment);
         if (activitiesAvailable) {
+            document.getElementById("activity-tab-feed").appendChild(fragment);
             trueActivityTab.style.display = "flex";
             trueActivityTab.classList.add("show-activity-tab");
             activityTitle.style.marginBottom = "0.6rem";
